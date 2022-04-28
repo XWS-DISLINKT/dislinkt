88087fe67b089ec06477db756fdd33d9c4d49043
MilosZivic75 <zivic.ra75.2018@uns.ac.rs>
2022-04-28 20:44:44 +0200

    Post service added

---
+++ generate.sh
@@ -0,0 +1,22 @@
+#!/bin/(shell)
+
+full_path=$1
+files=$(echo $full_path | tr "/" " ")
+files=($files)
+path=""
+
+declare -i i=1
+for file in ${files[@]}
+do
+    if [ ${#files[@]} -gt $i ]
+    then
+        path="$path/$file"
+    fi
+    i=$i+1
+done
+
+protoc -I "./common/proto$path" \
+       --go_out "./common/proto$path" --go_opt paths=source_relative \
+       --go-grpc_out "./common/proto$path" --go-grpc_opt paths=source_relative \
+       --grpc-gateway_out "./common/proto$path" --grpc-gateway_opt paths=source_relative \
+       "./common/proto$full_path"
